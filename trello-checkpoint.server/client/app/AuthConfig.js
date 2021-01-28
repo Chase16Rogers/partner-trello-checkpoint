76c6b8dcf397a21d073af9b3a51a536a82904af3
Chase16Rogers <chase16rogers@gmail.com>
2021-01-28 13:53:19 -0700

    real build for prod

--- trello-checkpoint.server/client/app/AuthConfig.js
+++
@@ -1,3 +0,0 @@
-export const domain = 'dev-c3yjh80u.us.auth0.com'
-export const clientId = 'IiDoDO9eV8ehAc19cWEP9i8imtt7juSF'
-export const audience = 'https://project-manager-checkpoint.herokuapp.com/'
