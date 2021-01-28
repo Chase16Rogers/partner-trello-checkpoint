5db7d6f12b9dec832bad6d795e493b732b427d30
Chase16Rogers <chase16rogers@gmail.com>
2021-01-28 13:48:44 -0700

    build for prod

--- trello-checkpoint.client/vue.config.js
+++ trello-checkpoint.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
- // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
+  // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: 'docs',
+  outputDir: '../trello-checkpoint.server/client',
   productionSourceMap: false
 }
